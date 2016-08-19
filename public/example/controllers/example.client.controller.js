28821f6a32a1771b746425a524b17fdd35fc74f5
Sean <sanelson87@gmail.com>
2016-08-19 09:36:18 +0100

    Added simple PDF download from participant button click

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -15,5 +15,24 @@ angular.module('example').controller('ExampleController', ['$scope', 'Authentica
         $scope.goToActivities = function(){
         	window.location = "/#!/activities"
         }
+
+        $scope.simplePDF = function() {
+            //    console.log("button clicked");
+            //  $scope.order.showPopupAddedToCart = !$scope.order.showPopupAddedToCart;
+            var doc = new jsPDF();
+            doc.text(20, 20, 'Hello world!');
+            doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+            doc.addPage();
+            doc.text(20, 20, 'Do you like that?');
+
+            // Save the PDF
+            doc.save('Test.pdf');
+
+
+
+
+        };
+
+
     }
 ]);
